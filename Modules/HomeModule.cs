0fdfca98db4c83b6ba29156fcb1da8883daf666a
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-27 10:10:25 -0700

    add form

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -15,6 +15,15 @@ namespace FriendLetter
         myLetterVariables.SetSender("John");
         return View["hello.cshtml", myLetterVariables];
       };
+      Get["/form"] = _ => {
+        return View["form.cshtml"];
+      };
+      Get["/greeting_card"] = _ => {
+        LetterVariables myLetterVariables = new LetterVariables();
+        myLetterVariables.SetRecipient(Request.Query["recipient"]);
+        myLetterVariables.SetSender(Request.Query["sender"]);
+        return View["hello.cshtml", myLetterVariables];
+      };
       Get["/favorite_photos"] = _ => View["favorite_photos.html"];
     }
   }
